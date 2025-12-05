5d7d397f4503e722eef98c11e6a54f76b4a8ef93
A1med_YUUUUUU <Almoonex@outlook.com>
2025-12-05 23:03:58 +0800

    配置

--- slidev.config.ts
+++ slidev.config.ts
@@ -1,10 +1,7 @@
 import { defineSlidevConfig } from '@slidev/types'
 
 export default defineSlidevConfig({
-  // 如果部署到 GitHub Pages 的子目录，需要设置 base
-  // 例如：如果仓库名是 pptx，URL 是 https://username.github.io/pptx/
-  // 则 base 应该是 '/pptx/'
-  // 如果部署到个人主页根目录 (username.github.io)，base 应该是 '/'
-  base: process.env.BASE_PATH || '/',
+  // GitHub Pages 部署路径：仓库名是 pptx，所以 base 是 /pptx/
+  base: '/pptx/',
 })
 
